62ef2b1eea1a9656906b5c0438313e3926d2701b
Brendan DeLeeuw <brendandeleeuw@gmail.com>
2022-05-14 17:22:29 -0600

    Starting to setup the main harf with all the actual har specs

--- src/harf.py
+++ src/harf.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from typing import TypeVar, Generic, Callable, List, Union, cast, Any
+from functools import partial
+from typing import TypeVar, Generic, Callable, List, Union, cast, Any, Optional
 
 from serde import serde, SerdeSkip, field
 from serde.json import from_json, to_json, JsonSerializer, JsonDeserializer
@@ -8,6 +9,127 @@ A = TypeVar("A")
 B = TypeVar("B")
 C = TypeVar("C")
 D = TypeVar("D")
+
+W = TypeVar("W")
+X = TypeVar("X")
+Y = TypeVar("Y")
+Z = TypeVar("Z")
+
+oint = Optional[int]
+ostr = Optional[str]
+obool = Optional[bool]
+F = Callable[[A], B]
+
+
+dint = partial(field, default=-1)
+
+
+@serde
+@dataclass
+class TimingsF(Generic[A, B, C, D]):
+    send: int
+    wait: int
+    receive: int
+    blocked: oint = dint()
+    dns: oint = dint()
+    connect: oint = dint()
+    ssl: oint = dint()
+    comment: ostr = ""
+
+    def nmap(
+        self: "TimingsF[A, B, C, D]",
+        f: F[A, W],
+        g: F[B, X],
+        h: F[C, Y],
+        i: F[D, Z],
+    ) -> "TimingsF[W, X, Y, Z]":
+        return cast(TimingsF[W, X, Y, Z], self)
+
+
+@serde
+@dataclass
+class BeforeAfterRequestF(Generic[A, B, C, D]):
+    lastAccess: str
+    eTag: str
+    hitCount: int
+    expires: ostr = None
+    commint: ostr = ""
+
+    def nmap(
+        self: "BeforeAfterRequestF[A, B, C, D]",
+        f: F[A, W],
+        g: F[B, X],
+        h: F[C, Y],
+        i: F[D, Z],
+    ) -> "BeforeAfterRequestF[W, X, Y, Z]":
+        return cast(BeforeAfterRequestF[W, X, Y, Z], self)
+
+
+@serde
+@dataclass
+class CacheF(Generic[A, B, C, D]):
+    beforeRequest: A
+    afterRequest: B
+
+    def namp(
+        self: "CacheF[A, B, C, D]",
+        f: F[A, W],
+        g: F[B, X],
+        h: F[C, Y],
+        i: F[D, Z],
+    ) -> "CacheF[W, X, Y, Z]":
+        return CacheF(f(self.beforeRequest), g(self.afterRequest))
+
+
+@serde
+@dataclass
+class ContentF(Generic[A, B, C, D]):
+    size: int
+    mimeType: str
+    compression: oint = None
+    text: ostr = ""
+    encoding: ostr = ""
+    comment: ostr = ""
+
+    def nmap(
+        self: "ContentF[A, B, C, D]",
+        f: F[A, W],
+        g: F[B, X],
+        h: F[C, Y],
+        i: F[D, Z],
+    ) -> "ContentF[W, X, Y, Z]":
+        return cast(ContentF[W, X, Y, Z], self)
+
+
+@serde
+@dataclass
+class HeaderF(Generic[A, B]):
+    name: str
+    value: str
+    comment: ostr = ""
+
+    def nmap(self: "HeaderF[A, B]", f: F[A, C], g: F[B, D]) -> "HeaderF[C, D]":
+        return cast(HeaderF[C, D], self)
+
+
+@serde
+@dataclass
+class CookieF(Generic[A, B]):
+    name: str
+    value: str
+    path: ostr = None
+    domain: ostr = None
+    expired: ostr = None
+    httpOnly: obool = None
+    secure: obool = None
+    comment: ostr = ""
+
+    def nmap(
+        self: "CookieF[A, B]",
+        f: Callable[[A], C],
+        g: Callable[[B], D],
+    ) -> "CookieF[C, D]":
+        return cast(CookieF[C, D], self)
 
 
 @serde
@@ -22,26 +144,6 @@ class QueryStringF(Generic[A, B]):
         g: Callable[[B], D],
     ) -> "QueryStringF[C, D]":
         return cast(QueryStringF[C, D], self)
-
-    def __str__(self):
-        return f"QueryStringF(name={self.name}, value={self.value})"
-
-
-@serde
-@dataclass
-class CookieF(Generic[A, B]):
-    name: str
-    value: str
-
-    def nmap(
-        self: "CookieF[A, B]",
-        f: Callable[[A], C],
-        g: Callable[[B], D],
-    ) -> "CookieF[C, D]":
-        return cast(CookieF[C, D], self)
-
-    def __str__(self):
-        return f"CookieF(name={self.name}, value={self.value})"
 
 
 @serde
