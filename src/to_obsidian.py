759543fdc702809e05a78f124ec29b1aa5ffd795
Brendan DeLeeuw <brendandeleeuw@gmail.com>
2022-06-06 22:38:51 -0600

    Adding a dataclass pretty printer

--- src/to_obsidian.py
+++ src/to_obsidian.py
@@ -1,7 +1,57 @@
 from serde.json import from_json
 
+from collections.abc import Mapping, Iterable
+from dataclasses import replace, is_dataclass, fields
 from harf import cata, Har, QueryStringF, HeaderF, RequestF, EntryF, LogF, TopF, HarF
-from typing import Any, NamedTuple
+from typing import Any, NamedTuple, Tuple, List
+from uuid import uuid4
+
+
+def pretty_print(obj, indent=4):
+    """
+    Pretty prints a (possibly deeply-nested) dataclass.
+    Each new block will be indented by `indent` spaces (default is 4).
+    """
+    print(stringify(obj, indent))
+
+def stringify(obj, indent=4, _indents=0):
+    if isinstance(obj, str):
+        return f"'{obj}'"
+
+    if not is_dataclass(obj) and not isinstance(obj, (Mapping, Iterable)):
+        return str(obj)
+
+    this_indent = indent * _indents * ' '
+    next_indent = indent * (_indents + 1) * ' '
+    start, end = f'{type(obj).__name__}(', ')'  # dicts, lists, and tuples will re-assign this
+
+    if is_dataclass(obj):
+        body = '\n'.join(
+            f'{next_indent}{field.name}='
+            f'{stringify(getattr(obj, field.name), indent, _indents + 1)},' for field in fields(obj)
+        )
+
+    elif isinstance(obj, Mapping):
+        if isinstance(obj, dict):
+            start, end = '{}'
+
+        body = '\n'.join(
+            f'{next_indent}{stringify(key, indent, _indents + 1)}: '
+            f'{stringify(value, indent, _indents + 1)},' for key, value in obj.items()
+        )
+
+    else:  # is Iterable
+        if isinstance(obj, list):
+            start, end = '[]'
+        elif isinstance(obj, tuple):
+            start = '('
+
+        body = '\n'.join(
+            f'{next_indent}{stringify(item, indent, _indents + 1)},' for item in obj
+        )
+
+    return f'{start}\n{body}\n{this_indent}{end}'
+
 
 with open("test/www.demoblaze.com_Archive [22-05-30 13-47-04].har") as file:
     data = from_json(Har, file.read())
@@ -46,4 +96,4 @@ def get_values(element: HarF[Tuple[HarF, Env]]) -> Tuple[HarF, Env]:
     return element, []
 
 
-print(data.log.entries[2])
+pretty_print(cata(get_values, data))
