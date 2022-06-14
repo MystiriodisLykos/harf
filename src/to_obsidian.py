ba8b947e99046a60f50483a37eb77480ec71f3c6
Brendan DeLeeuw <brendandeleeuw@gmail.com>
2022-06-13 22:48:04 -0600

    Fully creating an env

--- src/to_obsidian.py
+++ src/to_obsidian.py
@@ -2,10 +2,11 @@ from serde.json import from_json
 
 from collections.abc import Mapping, Iterable
 from dataclasses import replace, is_dataclass, fields
-from harf import cata, Har, QueryStringF, HeaderF, RequestF, ResponseF, EntryF, LogF, TopF, HarF, PostDataTextF
-from typing import Any, NamedTuple, Tuple, List, TypeVar, Generic, Callable
+from harf import cata, Har, QueryStringF, HeaderF, RequestF, ResponseF, EntryF, LogF, TopF, HarF, PostDataTextF, ContentF
+from typing import Any, NamedTuple, Tuple, List, TypeVar, Generic, Callable, Union
 from uuid import uuid4
 from urllib.parse import urlparse
+import json
 
 A = TypeVar("A")
 S = TypeVar("S")
@@ -56,17 +57,17 @@ def stringify(obj, indent=4, _indents=0):
 
     return f'{start}\n{body}\n{this_indent}{end}'
 
+def json_fmap(source, f):
+    if isinstance(source, list):
+        return [f(e) for e in source]
+    if isinstance(source, dict):
+        return {k: f(v) for k, v in source.items()}
+    return source
 
 def json_cata(algebra: Callable[[A], R], source: S) -> R:
     def inner(element):
         return json_cata(algebra, element)
-    if isinstance(source, list):
-        results = [inner(e) for e in source]
-    elif isinstance(source, dict):
-        results = {k: inner(e) for k, e in source.items()}
-    else:
-        results = source
-    return algebra(results)
+    return algebra(json_fmap(source, inner))
 
 
 
@@ -74,6 +75,7 @@ class Thunk(Generic[A]):
     _thunk_indices = []
     _thunk_values = []
     def __new__(cls, value: A):
+        value=str(value)
         try:
             thunk_index = cls._thunk_indices.index(value)
             return cls._thunk_values[thunk_index]
@@ -87,7 +89,45 @@ class Thunk(Generic[A]):
     def __eq__(self, other):
         return self._value == other._value
     def __repr__(self):
-        return f"Thunk({self._value!r})"
+        return f"Thunk({self._value!r})|{id(self)}"
+
+class JsonIndexId(NamedTuple):
+    e: int
+    next_: "JsonId"
+
+class JsonStringId(NamedTuple):
+    e: str
+    next_: "JsonId"
+
+JsonId = Union[JsonIndexId, JsonStringId, None]
+
+class UrlId(NamedTuple):
+    next_: JsonId
+
+class PostDataId(NamedTuple):
+    next_: JsonId
+
+class HeaderId(NamedTuple):
+    next_: JsonId
+
+class CookieId(NamedTuple):
+    next_: JsonId
+
+class QueryStringId(NamedTuple):
+    next_: JsonId
+
+class RequestId(NamedTuple):
+    e: int
+    next_: Union[UrlId, PostDataId, HeaderId, CookieId, QueryStringId]
+
+class ContentId(NamedTuple):
+    next_: JsonId
+
+class ResponseId(NamedTuple):
+    e: int
+    next_: Union[ContentId, HeaderId, CookieId]
+
+Reference = Union[RequestId, ResponseId]
 
 class EnvE(NamedTuple):
     name: str
@@ -97,36 +137,44 @@ Env = List[EnvE]
 
 
 def json_env(json) -> Env:
+    original = json_fmap(json, lambda e: e[1])
+    sub_envs = json_fmap(json, lambda e: e[0])
     if isinstance(json, list):
-        iterator = map(lambda e: (f"[{len(json)-e[0]-1}]", e[1]), enumerate(reversed(json)))
+        ids = map(lambda e: f"[{e}]", range(len(sub_envs)))
     elif isinstance(json, dict):
-        iterator = map(lambda e: (f".{e[0]}", e[1]), reversed(json.items()))
+        ids = map(lambda e: f".{e}", original.keys())
+        sub_envs = sub_envs.values()
     else:
-        return [EnvE("", Thunk(json))]
+        return [EnvE("", Thunk(original))], Thunk(original)
     env = []
-    for k, v in iterator:
-        sub_env = list(map(lambda e: EnvE(k + e.name, e.value), v))
-        env += sub_env
-    return EnvE("", Thunk(json)) + env
+    for id_, sub_env in zip(ids, sub_envs):
+        sub_env = list(map(lambda e: EnvE(id_ + e.name, e.value), sub_env))
+        env = sub_env + env
+    return [EnvE("", Thunk(original))] + env, Thunk(original)
 
 def build_env(element: HarF[Env]) -> Env:
     if isinstance(element, PostDataTextF):
-        return json_cata(json_env, element)
+        return json_cata(json_env, json.loads(element.text))[0]
     if isinstance(element, RequestF):
         path = urlparse(element.url).path.strip("/").split("/")
         env = []
         for i, path_part in enumerate(reversed(path)):
             env += [EnvE(f"request.path[{len(path)-i-1}]", Thunk(path_part))]
-        return [EnvE(f"request.postData{e.name}", e.value) for e in element.postData] + env
-    #if isinstance(element, ContentF):
-    #    TODO: json
-#    if isinstance(element, ResponseF):
-#        return response.content
+        if element.postData != None:
+            return [EnvE(f"request.postData{e.name}", e.value) for e in element.postData] + env
+        return env
+    if isinstance(element, ContentF):
+        if element.text != "":
+            return json_cata(json_env, json.loads(element.text))[0]
+    if isinstance(element, ResponseF):
+        return [EnvE(f"response.content{e.name}", e.value) for e in element.content]
     if isinstance(element, EntryF):
-        print(element.request + element.response)
-        return element.request + element.response
+        return element.response + element.request
     if isinstance(element, LogF):
-        return element.entries
+        env = []
+        for i, sub_envs in enumerate(element.entries):
+            env = [EnvE(f"{i+1}.{e.name}", e.value) for e in sub_envs] + env
+        return env
     if isinstance(element, TopF):
         return element.log
     return []
