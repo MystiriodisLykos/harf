b1f6caeddacddf4a52766d976ba91a56a748668d
Brendan DeLeeuw <brendandeleeuw@gmail.com>
2022-07-21 23:20:59 -0600

    Switching to path objects instead of a raw string

--- src/correlations.py
+++ src/correlations.py
@@ -1,4 +1,6 @@
-from typing import Set, NamedTuple, Iterable, Any, List
+from dataclasses import dataclass
+from functools import partial
+from typing import Set, NamedTuple, Iterable, Any, List, NewType, Union, Protocol
 from urllib.parse import urlparse
 from itertools import chain
 
@@ -13,20 +15,52 @@ class Correlation(NamedTuple):
     def __hash__(self):
         return hash(self.value)
 
-def names(element: HarF[List[str]]) -> List[str]:
+class Path(Protocol):
+    next_: "Path"
+
+@dataclass
+class IntPath:
+    index: int
+    next_: "DataPath"
+
+@dataclass
+class StrPath:
+    name: str
+    next_: "DataPath"
+
+DataPath = Union[IntPath, StrPath, None]
+
+class UrlPath(IntPath): pass
+class QueryPath(StrPath): pass
+
+@dataclass
+class BodyPath:
+    next_: DataPath
+
+@dataclass
+class RequestPath:
+    next_: Union[UrlPath, QueryPath, BodyPath]
+
+class ResponsePath(BodyPath): pass
+
+@dataclass
+class EntryPath:
+   index: int
+   next_: Union[RequestPath, ResponsePath]
+
+def paths(element: HarF[List[Path]]) -> List[Path]:
     if isinstance(element, RequestF):
         path = urlparse(element.url).path.strip("/").split("/")
-        path_names = []
-        for i, part in enumerate(path):
-            path_names.append(f"path[{i}]")
-        return path_names
+        paths = []
+        for i in range(len(path)):
+            paths.append(RequestPath(UrlPath(i, None)))
+        return paths
     if isinstance(element, EntryF):
-        return ["request"] + list(map(lambda e: f"request.{e}", element.request))
+        return element.request
     if isinstance(element, LogF):
         results = []
         for i, entry in enumerate(element.entries):
-            for name in entry:
-                results.append(f"[{i}]{name}")
+            results.extend(map(lambda e: EntryPath(i, e), entry))
         return results
     if isinstance(element, TopF):
         return element.log
@@ -47,4 +81,4 @@ def names(element: HarF[List[str]]) -> List[str]:
 from pprint import pprint
 with open("test/example1.har") as file:
     har = from_json(Har, file.read())
-    pprint(cata(names, har))
+    pprint(cata(paths, har))
