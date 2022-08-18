4b81fa15789f2069899839fe30f112fee0af9b49
Brendan DeLeeuw <brendandeleeuw@gmail.com>
2022-08-17 21:49:08 -0600

    Moving env creation to cli so it can determine what parts of the env need to be made

--- harf/cli.py
+++ harf/cli.py
@@ -5,8 +5,17 @@ from pprint import pprint
 import click
 from serde.json import from_json
 
-from harf.core import Har
-from harf.correlations import mk_env
+from harf.core import Har, harf
+from harf.correlations import (
+    post_data_env,
+    header_env,
+    content_env,
+    response_env,
+    request_env,
+    entry_env,
+    log_env,
+    Env,
+)
 
 
 @click.command()
@@ -18,7 +27,16 @@ def correlations(har_file, interactive, diffable, headers):
     # todo: add filters for number of references
     # todo: add verbose to show url instead of entry_{i}
     har = from_json(Har, har_file.read())
-    env = mk_env(har)
+    env = harf(
+        post_data=post_data_env,
+        header=header_env if headers else lambda *a, **k: Env(),
+        content=content_env,
+        response=response_env,
+        request=request_env,
+        entry=entry_env,
+        log=log_env,
+        default=Env(),
+    )(har)
     if interactive:
         code.interact(local={"env": env})
     else:
