083f33ce7698d894d2915911a7e10dcdb615bde4
Brendan DeLeeuw <brendandeleeuw@gmail.com>
2022-08-30 21:57:35 -0600

    Working on harf stategies

--- tests/strategies.py
+++ tests/strategies.py
@@ -1,37 +1,55 @@
+from typing import get_args
 from string import printable
+
 from hypothesis import strategies as st
 
-from harf.core import TimingsF, BeforeAfterRequestF, CacheF, ContentF
+from harf.core import TimingsF, BeforeAfterRequestF, CacheF, Content, Cache
 
 text = st.text(printable)
+comment = text | st.none()
 positive_int = st.integers(0)
 optional_int = positive_int | st.just(-1) | st.none()
 datetime = st.datetimes().map(lambda d: d.strftime("%Y-%m-%dT%H:%M:%S"))
 
-def timings(t):
-    """ Generates TimingsF objects with positive and optional values
+timings = st.builds(
+    TimingsF,
+    send=positive_int,
+    wait=positive_int,
+    receive=positive_int,
+    ssl=optional_int,
+    connect=optional_int,
+    blocked=optional_int,
+    dns=optional_int,
+    comment=comment,
+)
 
-    The relation between `ssl` and `connect` is not modeled
-    """
+st.register_type_strategy(TimingsF, timings)
+
+before_after_request = st.builds(
+    BeforeAfterRequestF,
+    eTag=text,
+    lastAccess=datetime,
+    hitCount=positive_int,
+    expires=datetime | st.none(),
+    comment=comment,
+)
+
+st.register_type_strategy(BeforeAfterRequestF, before_after_request)
+
+def cache(t):
+    try:
+        before, after = get_args(t)
+    except ValueError as e:
+        raise ValueError (f"Cannot create generic type {t}") from e
+    print(before, after)
     return st.builds(
-        TimingsF,
-        send=positive_int,
-        wait=positive_int,
-        receive=positive_int,
-        ssl=optional_int,
-        connect=optional_int,
-        blocked=optional_int,
-        dns=optional_int,
-        comment=text,
+        CacheF,
+        beforeRequest=st.from_type(before) | st.none(),
+        afterRequest=st.from_type(after) | st.none(),
+        comment=comment,
     )
 
-def before_after_request(t):
-    return st.builds(
-        BeforeAfterRequestF,
-        eTag=text,
-        expires=datetime,
-        lastAccess=datetime,
-        hitCount=positive_int,
-        comment=text,
-    )
+st.register_type_strategy(CacheF, cache)
 
+def content(t):
+    
