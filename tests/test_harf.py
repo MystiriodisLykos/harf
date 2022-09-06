c40dcb3ed33ba9a3d345eccd4e8e71abf9650807
Brendan DeLeeuw <brendandeleeuw@gmail.com>
2022-09-05 22:36:21 -0600

    Working on some basic json tests

--- tests/test_harf.py
+++ tests/test_harf.py
@@ -3,37 +3,31 @@ from inspect import signature
 from string import printable
 from typing import get_args, TypeVar, List
 
-from hypothesis import assume, given, infer, note, strategies as st
+from hypothesis import assume, example, given, infer, note, strategies as st
 
 from harf.core import harf, Har, ResponseF, Response, CookieF
 from harf.correlations import mk_env, json_env
 
-text = st.text(printable)
-
-json_prims = st.none() | st.booleans() | st.floats() | text | st.integers()
-
-
-@st.composite
-def json(draw, prims=json_prims, keys=text, min_size=0, max_size=None):
-    return draw(
-        st.recursive(
-            prims,
-            lambda children: st.lists(children, min_size=min_size, max_size=max_size)
-            | st.dictionaries(keys, children, min_size=min_size, max_size=max_size),
-        )
-    )
-
+from strategies import json_prims, json
 
 @given(json=json_prims.flatmap(lambda e: json(prims=st.just(e), min_size=1)))
+@example(None)
 def test_jsonn_with_single_prim_makes_env_with_single_entry(json):
     env = json_env(json)
     note(env)
     assert len(env) == 1
 
 
-test_jsonn_with_single_prim_makes_env_with_single_entry()
+# test_jsonn_with_single_prim_makes_env_with_single_entry()
 
-# @given(default=json, har=infer)
-def test_harf_default_is_default(default: int, har: Response):
-    assert default == harf(default=default)(har)
-    pass
+"""
+@given(alphabet=st.shared(st.sets(json_prims), key="alphabet"),json=json(prims=st.shared(st.sets(json_prims), key="alphabet").flat_map(lambda e: st.sampled_from(list(e)))))
+def test_json_with_alphabet_A_makes_env_with_A_or_less(alphabet, json):
+    note(alphabet)
+    note(json)
+    assert len(alphabet) < 3
+"""
+# test_json_with_alphabet_A_makes_env_with_A_or_less()
+
+print(json(st.sets(st.integers())).example())
+
